fc21c3967af65d390380a899ed19075adf4b42b0
aoneill <aoneill@cartrawler.com>
2017-08-11 16:22:45 +0100

    init

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  API_KEY: "AIzaSyCRROASNisMgkRXEx2BU-Bi99WvdOHnIk0",
+  AUTH_DOMAIN: "groceries-93d88.firebaseapp.com",
+  DATABASE_URL: "https://groceries-93d88.firebaseio.com",
+  STORAGE_BUCKET: "groceries-93d88.appspot.com"
+}
